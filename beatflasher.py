db6ddfd28a7de69a60f90a3eb25f96429a53a0ab
Rana Jurjus <froggyrulz1@gmail.com>
2021-04-24 14:42:27 -0400

    Scoring and results
    Added score and other realtime information to the game mode. Also added a results page that includes a graph of the combo over time.

--- beatflasher.py
+++ beatflasher.py
@@ -244,45 +244,141 @@ def loadLevel(app, level):
     app.mode = "gameMode"
     app.readyToStart = True
     app.paused = True
+    app.elapsed = 0
+    app.progress = 0
+    app.missedIndex = 0
     app.notesToDisplay = []
-    app.currIndex = 0
+    app.noteScores = [None for note in app.currLevel]
+    app.score = 0
+    app.combo = 0
+    app.max_combo = 0
+    app.misses = [] # Store the times that the misses occurred.
+
+def getNotesWithinTimeRange(notes, startTime, endTime):
+    low = 0
+    high = len(notes)
+    while low < high:
+        mid = (low + high) // 2
+        if notes[mid][1] < startTime:
+            low = mid + 1
+        elif notes[mid][1] > startTime:
+            high = mid
+        else:
+            low = mid
+            high = mid + 1
+    startIndex = low
+    low = startIndex
+    high = len(notes)
+    while low < high:
+        mid = (low + high) // 2
+        if notes[mid][1] <= endTime:
+            low = mid + 1
+        elif notes[mid][1] > endTime:
+            high = mid
+    endIndex = high
+    return startIndex, endIndex
 
 def getNotesWithinSeconds(app, seconds):
     currTime = time.time()
     elapsed = currTime - app.startTime
-    notes = []
-    # Scan past notes that have already occurred.
-    while (app.currIndex < len(app.currLevel) and
-            app.currLevel[app.currIndex][1] < elapsed):
-        app.currIndex += 1
-    i = app.currIndex
-    while (i < len(app.currLevel) and
-            app.currLevel[i][1] - elapsed < seconds):
-        direction, startTime, endTime = app.currLevel[i]
-        timeUntilNote = startTime - elapsed
-        if timeUntilNote > 0:
-            notes.append((direction, timeUntilNote / seconds))
-        i += 1
-    return notes
+    startIndex, endIndex = getNotesWithinTimeRange(app.currLevel,
+                                                    elapsed, elapsed + seconds)
+    app.progress = startIndex / len(app.currLevel)
+    notes = app.currLevel[startIndex:endIndex]
+    directionsWithProportions = [(direction, (startTime - elapsed) / seconds)
+                                 for (direction, startTime, endTime) in notes]
+    return directionsWithProportions
 
 def gameMode_timerFired(app):
     if app.paused:
         return None
+    elapsed = time.time() - app.startTime
+    app.elapsed = elapsed
     speedToSecondsOnPage = {1: 5, 2: 4, 3: 3, 4: 2, 5: 1, 11: 0.1}
     secondsOnPage = speedToSecondsOnPage[app.arrowSpeed]
     app.notesToDisplay = getNotesWithinSeconds(app, secondsOnPage)
+    while (app.missedIndex < len(app.currLevel) and
+            app.currLevel[app.missedIndex][1] < elapsed - 0.2):
+        if app.noteScores[app.missedIndex] == None:
+            combo_multiplier = 1 + min(app.combo * 0.1, 3)
+            app.score -= 5
+            app.combo = 0
+            app.noteScores[app.missedIndex] = ('Miss',
+                                    combo_multiplier, app.combo, app.score, elapsed)
+        app.missedIndex += 1
+    if app.missedIndex == len(app.currLevel):
+        loadResultsPage(app)
 
 def gameMode_keyPressed(app, event):
     if app.readyToStart:
         app.readyToStart = False
         app.paused = False
         app.startTime = time.time()
+        return None
+    if event.key in app.keyBinds:
+        elapsed = time.time() - app.startTime
+        keyDirection = app.keyBinds[event.key]
+        threshold = 0.2
+        startIndex, endIndex = getNotesWithinTimeRange(app.currLevel,
+                                elapsed - threshold, elapsed + threshold)
+        bestNoteIndex = None
+        bestError = None
+        for i in range(startIndex, endIndex):
+            direction, startTime, endTime = app.currLevel[i]
+            if app.noteScores[i] == None and direction == keyDirection:
+                currError = abs(startTime - elapsed)
+                if bestError == None or currError < bestError:
+                    bestNoteIndex = i
+                    bestError = currError
+        combo_multiplier = 1 + min(app.combo * 0.1, 3)
+        if bestError == None:
+            app.score -= 5
+            app.misses.append((elapsed, app.score))
+            app.combo = 0
+        elif bestError < 0.05:
+            app.score += 10 * combo_multiplier
+            app.combo += 1
+            app.noteScores[bestNoteIndex] = ('Perfect',
+                                    combo_multiplier, app.combo, app.score, elapsed)
+        elif bestError < 0.1:
+            app.score += 5 * combo_multiplier
+            app.combo += 1
+            app.noteScores[bestNoteIndex] = ('Good',
+                                combo_multiplier, app.combo, app.score, elapsed)
+        else:
+            app.score += 2 * combo_multiplier
+            app.combo = 0
+            app.noteScores[bestNoteIndex] = ('OK', 
+                                    combo_multiplier, app.combo, app.score, elapsed)
+        app.max_combo = max(app.combo, app.max_combo)
 
-def gameMode_redrawAll(app,canvas):
+def drawTopBar(app, canvas):
+    score_x = 0
+    score_y = 20
+    canvas.create_text(score_x, score_y,
+                        anchor='w',
+                        font='Arial 16 bold',
+                        text=f'Score: {int(app.score):,}')
+    
+    combo_x = app.width / 2
+    combo_y = 20
+    canvas.create_text(combo_x, combo_y,
+                        font='Arial 16 bold',
+                        text=f'Combo: {app.combo}')
+
+    progress_x = app.width
+    progress_y = 20
+    canvas.create_text(progress_x, progress_y,
+                        anchor='e',
+                        font='Arial 16 bold',
+                        text=f'P: {int(100 * app.progress)}%')
+
+def gameMode_redrawAll(app, canvas):
     if app.readyToStart:
         canvas.create_text(app.width/2, app.height/2, 
                     text="Press any key to start.", font="Arial 30 bold")
         return None
+    drawTopBar(app, canvas)
     arrowHeight = 60
     arrowWidth = 50
     verticalCenter = 100
@@ -373,6 +469,121 @@ def gameMode_redrawAll(app,canvas):
 # Results Page 
 ###################################################
 
+def loadResultsPage(app):
+    app.mode = 'resultsPage'
+    app.counts = {
+            'Perfect': 0,
+            'Good': 0,
+            'OK': 0,
+            'Miss': 0,
+            'Total': len(app.currLevel)
+        }
+    for score, _, _, _, _ in app.noteScores:
+        app.counts[score] += 1
+
+def resultsPage_mouseClicked(app, event):
+    pass
+
+def getTimeFromScore(score):
+    return score[4]
+
+def getTimeFromMiss(miss):
+    return miss[0]
+
+def mergeNoteScoresAndMisses(app):
+    scoresIndex = 0
+    missesIndex = 0
+    result = []
+    sortedScores = sorted(app.noteScores, key=getTimeFromScore)
+    sortedMisses = sorted(app.misses, key=getTimeFromMiss)
+    while scoresIndex < len(sortedScores) and missesIndex < len(sortedMisses):
+        _, _, combo, _, noteTime = sortedScores[scoresIndex]
+        missTime, _ = sortedMisses[missesIndex]
+        if noteTime < missTime:
+            result.append((noteTime, combo))
+            scoresIndex += 1
+        elif missTime < noteTime:
+            result.append((missTime, 0))
+            missesIndex += 1
+        else: # This should never happen (fingers crossed).
+            assert(False)
+    while scoresIndex < len(sortedScores):
+        _, _, combo, _, noteTime = sortedScores[scoresIndex]
+        result.append((noteTime, combo))
+        scoresIndex += 1
+    while missesIndex < len(sortedMisses):
+        missTime, _ = sortedMisses[missesIndex]
+        result.append((missTime, combo))
+        missesIndex += 1
+    return result
+
+def drawGraph(app, canvas):
+    x1 = 30
+    y1 = 30
+    x2 = app.width - 30
+    y2 = app.height / 3 - 30
+    canvas.create_rectangle(x1, y1, x2, y2)
+    mergedEvents = mergeNoteScoresAndMisses(app)
+    if len(mergedEvents) <= 1:
+        canvas.create_text(app.width / 2, (y1 + y2) / 2,
+                            font='Arial 20 bold',
+                            text='Wow, that was a cool level.')
+        return None
+    maxTime = app.elapsed
+    maxCombo = app.max_combo
+    firstTime, firstCombo = mergedEvents[0]
+    lastX = x1 + (x2 - x1) * (firstTime / maxTime)
+    lastY = y2 - (y2 - y1) * (firstCombo / maxCombo)
+    canvas.create_line(x1, y2, lastX, lastY)
+    for nextTime, nextCombo in mergedEvents[1:]:
+        nextX = x1 + (x2 - x1) * (nextTime / maxTime)
+        nextY = y2 - (y2 - y1) * (nextCombo / maxCombo)
+        canvas.create_line(lastX, lastY, nextX, nextY)
+        lastX = nextX
+        lastY = nextY
+    canvas.create_line(lastX, lastY, x2, lastY)
+
+def drawTotals(app, canvas):
+    top_y = app.height / 3
+    bot_y = 2 * app.height / 3
+    left = 10
+    score_y = top_y + (bot_y - top_y) / 6
+    canvas.create_text(left, score_y,
+                        anchor='w',
+                        font='Arial 16 bold',
+                        text=f'Total Score: {int(app.score):,}')
+    time_y = top_y + 3 * (bot_y - top_y) / 6
+    canvas.create_text(left, time_y,
+                        anchor='w',
+                        font='Arial 16 bold',
+                        text=f'Total Time: {int(app.elapsed)}s')
+    combo_y = top_y + 5 * (bot_y - top_y) / 6
+    canvas.create_text(left, combo_y,
+                        anchor='w',
+                        font='Arial 16 bold',
+                        text=f'Highest Combo: {app.max_combo}')
+
+def drawButtons(app, canvas):
+    pass
+
+def drawCounts(app, canvas):
+    top_y = app.height / 3
+    bot_y = 3 * app.height / 4
+    right = app.width - 20
+    countsList = [(score, app.counts[score]) for score in app.counts]
+    for i in range(len(countsList)):
+        score, count = countsList[i]
+        y = top_y + (1 + 2 * i) * (bot_y - top_y) / (2 * len(countsList))
+        canvas.create_text(right, y,
+                        anchor='e',
+                        font='Arial 16 bold',
+                        text=f'{score}: {count}')
+
+def resultsPage_redrawAll(app, canvas):
+    drawGraph(app, canvas)
+    drawTotals(app, canvas)
+    drawButtons(app, canvas)
+    drawCounts(app, canvas)
 
 ###################################################
 # Main App 
@@ -384,13 +595,13 @@ def appStarted(app):
     app.displayHelp = False
     app.levelsPerPage = 5
     app.arrowDirection = 'Up'
-    app.arrowSpeed = 11
+    app.arrowSpeed = 3
     app.arrowShape = 'Triangle'
     app.keyBinds = {
-            'left': 'Left',
-            'right': 'Right',
-            'up': 'Up',
-            'down': 'Down'
+            'Left': 'left',
+            'Right': 'right',
+            'Up': 'up',
+            'Down': 'down'
         }
     app.timerDelay = 25
 
